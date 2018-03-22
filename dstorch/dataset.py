e3ef4f809656719c7f023c37e1066bc683ec4bb7
amirassov <amirassov@gmail.com>
2018-03-22 21:28:10 +0300

    blabla

--- dstorch/dataset.py
+++ dstorch/dataset.py
@@ -3,45 +3,43 @@ import torch
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 from dstorch.utils import to_float_tensor, pad_image
+import cv2
 
 
 class BowlDataset(Dataset):
-    def __init__(self, images, masks, ids, transform, mode, period):
-        self.images = images
-        self.masks = masks
-        self.ids = ids
+    def __init__(self, filenames, path_image, path_mask, transform, mode, period):
+        self.filenames = filenames
+        self.path_image = path_image
+        self.path_mask = path_mask
         self.transform = transform
         self.mode = mode
         self.period = period
 
     def __len__(self):
-        return len(self.images)
+        return len(self.filenames)
 
     def __getitem__(self, index):
-        img = self.images[index]
-        mask = None if self.masks is None else self.masks[index]
+        filename = self.filenames[index]
+        img = cv2.imread(self.path_image.format(filename))
+        mask = None if self.mode == 'predict' else cv2.imread(self.path_mask.format(filename))
         img, mask = self.transform(img, mask)
 
         if self.mode == 'train':
             return to_float_tensor(img), to_float_tensor(mask)
         elif self.mode == 'validation':
             pad_img, top, left = pad_image(img, self.period)
-            pad_mask = pad_image(mask, self.period)[0]
+            pad_mask, top, left = pad_image(mask, self.period)
             return to_float_tensor(pad_img), to_float_tensor(pad_mask)
         elif self.mode == 'predict':
             pad_img, top, left = pad_image(img, self.period)
-            return to_float_tensor(pad_img), str(self.ids[index]), top, left
+            return to_float_tensor(pad_img), str(filename), top, left
         else:
             raise TypeError('Unknown mode type!')
 
-def make_loader(images, masks, ids, batch_size, transform, workers=1, shuffle=True, mode='train', period=128):
-    if images is None:
-        raise TypeError('Images must be a list!')
-    if ids is None:
-        raise TypeError('Ids must be a list!')
-
+def make_loader(filenames,  path_image, path_mask,
+                batch_size, transform, workers=1, shuffle=True, mode='train', period=128):
     return DataLoader(
-        dataset=BowlDataset(images, masks, ids, transform=transform, mode=mode, period=period),
+        dataset=BowlDataset(filenames, path_image, path_mask, transform=transform, mode=mode, period=period),
         shuffle=shuffle,
         num_workers=workers,
         batch_size=batch_size,
