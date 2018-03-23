647f1dafb8058fa1adbcc21eeb49c16164145d1b
amirassov <amirassov@gmail.com>
2018-03-23 13:29:20 +0300

    blabla

--- dstorch/io.py
+++ dstorch/io.py
@@ -1,7 +1,7 @@
 import os
 
 import numpy as np
-from skimage import io
+import cv2
 from tqdm import tqdm
 from skimage import img_as_bool, img_as_ubyte, img_as_float
 
@@ -13,11 +13,11 @@ def read_train_data(train_path, d=3):
 
     for id_ in tqdm(train_ids, desc='Reading train data..'):
         path = train_path + id_
-        image = img_as_ubyte(io.imread(path + '/images/' + id_ + '.png')[:, :, :d])
+        image = img_as_ubyte(cv2.imread(path + '/images/' + id_ + '.png')[:, :, :d])
         train_images.append(image)
         mask = None
         for mask_file in next(os.walk(path + '/masks/'))[2]:
-            mask_ = img_as_bool(io.imread(path + '/masks/' + mask_file))
+            mask_ = img_as_bool(cv2.imread(path + '/masks/' + mask_file))
             if len(mask_.shape) > 2:
                 mask_ = mask_[..., 0]
             if mask is None:
@@ -32,7 +32,7 @@ def read_test_data(test_path, d=3):
     
     for id_ in tqdm(test_ids, desc='Reading test data..'):
         path = test_path + id_
-        image = img_as_ubyte(io.imread(path + '/images/' + id_ + '.png')[:, :, :d])
+        image = img_as_ubyte(cv2.imread(path + '/images/' + id_ + '.png')[:, :, :d])
         test_images.append(image)
     return test_ids, test_images
 
