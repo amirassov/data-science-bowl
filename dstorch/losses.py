2702aa4aa7d9d1c312b47bf3024be5672c91045b
amirassov <amirassov@gmail.com>
2018-03-24 21:41:46 +0300

    blabla

--- dstorch/losses.py
+++ dstorch/losses.py
@@ -58,6 +58,6 @@ class BCEDiceLossMCC(nn.Module):
         loss = 0.3 * self.bce_dice(mask_input, mask_target)
         loss += 0.2 * self.bce_dice(contour_input, contour_target)
         loss += 0.3 * self.bce_dice(center_input, center_target)
-        loss += 0.1 * self.bce_dice(torch.mul(mask_input, (1.0 - contour_input)), center_target)
-        loss += 0.1 * self.bce_dice(torch.mul(mask_input, (1.0 - center_input)), contour_target)
+        loss += 0.1 * self.bce_dice(torch.div(torch.add(mask_input, 1.0 - contour_input), 2), center_target)
+        loss += 0.1 * self.bce_dice(torch.div(torch.add(mask_input, 1.0 - center_input), 2), contour_target)
         return loss / (0.3 + 0.2 + 0.3 + 0.1 + 0.1)
